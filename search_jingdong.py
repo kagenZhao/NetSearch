38885916438030419b13d3a40079d0c71cab6c57
kagenzhao <kagen@kagenz.com>
2018-06-04 13:04:16 +0800

    修复京东搜索错误

--- search_jingdong.py
+++ search_jingdong.py
@@ -6,12 +6,11 @@ from urllib.parse import quote
 class JingDongSearch(BaseSearch):
     def __init__(self, query):
         super(JingDongSearch, self).__init__(query)
-        self.reqeust_url = "https://dd-search.jd.com/?terminal=pc&newjson=1&ver=2&zip=1&curr_url=www.jd.com%2F&key=" + str(
-            self.query)
+        self.reqeust_url = "https://dd-search.jd.com/?terminal=pc&newjson=1&ver=2&zip=1&curr_url=www.jd.com%2F&key=" + str(self.query)
 
     def runDefault(self):
         result_arr = []
-        args = Args("https://search.jd.com/Search?keyword=%s" % quote(self.query), self.query)
+        args = Args("https://search.jd.com/Search?keyword=%s&enc=utf-8&wq=%s" % (quote(self.query), quote(self.query)), self.query)
         result_arr.append(Item(args.copy_text,
                                args.open_url,
                                '京东搜索 "%s"' % self.query,
